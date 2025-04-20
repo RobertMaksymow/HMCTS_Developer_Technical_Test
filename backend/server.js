e898a03c76f3fc9016ebfd7afba399e36bf5cf3b
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-04-20 19:37:37 +0100

    Added .env file, test middleware and test get route

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
+
+// MIDDLEWARE (code executed between getting a request and sending a response)
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next(); //this is mandatory otherwise the request will hang
+});
 
 // ROUTES
 app.get("/", (req, res) => {
   res.json({ message: "Hello from the backend!" });
 });
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
 });
