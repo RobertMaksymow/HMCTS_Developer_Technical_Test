6cfd3828fbcf552d7d169e1be40ebf4d18161114
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-04-21 22:39:39 +0100

    Basic React app ready. Should fetch data, but getting CORS issues while running locally.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [tasks, setTasks] = useState(null);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch("http://localhost:4000/api/tasks");
+      const json = await response.json();
+
+      if (response.ok) {
+        setTasks(json);
+      }
+    };
+
+    fetchTasks();
+  }, []);
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <div className="tasks">
+        {tasks &&
+          tasks.map((task) => (
+            <p key={task._id}>
+              {task.title} - {task.description}
+            </p>
+          ))}
+      </div>
     </div>
   );
 };
