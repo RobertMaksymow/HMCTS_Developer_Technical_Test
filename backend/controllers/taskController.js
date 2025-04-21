6971890de1a6e934bc92f2e76de56155251fac69
RobertMaksymow <114484269+RobertMaksymow@users.noreply.github.com>
2025-04-21 17:36:49 +0100

    Added UPDATE and DELETE calls. Works fine.

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -44,10 +44,38 @@ const createTask = async (req, res) => {
 };
 
 // PATCH a task
-const updateTaskByID = async () => {};
+const updateTaskByID = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Incorrect format of ID number" });
+  }
+
+  const task = await Task.findByIdAndUpdate({ _id: id }, { ...req.body });
+
+  if (!task) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+
+  res.status(200).json(task);
+};
 
 // DELETE a task
-const deleteTaskByID = async () => {};
+const deleteTaskByID = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Incorrect format of ID number" });
+  }
+
+  const task = await Task.findByIdAndDelete({ _id: id });
+
+  if (!task) {
+    return res.status(400).json({ error: "Task not found" });
+  }
+
+  res.status(200).json(task);
+};
 
 module.exports = {
   getAllTasks,
